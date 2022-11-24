c831775c4dd821d15b3295fda19810aa988a50f6
Luciaaf00 <luciaaf2000@gmail.com>
2022-11-24 01:21:07 +0100

    Continuación del formulario

---
+++ js/usuario.js
@@ -0,0 +1,23 @@
+var usuario ={
+    Nombre: document.getElementById(nombre),
+    PrimerApellido: document.getElementById(primer_apellido),
+    SegundoApellido: document.getElementById(segundo_apellido),
+    Email: document.getElementById(email),
+    Pais: document.getElementById(pais),
+    CodigoPostal: document.getElementById(codigo_postal),
+    Ciudad: document.getElementById(ciudad),
+    Provincia: document.getElementById(provincia),
+    Direccion: document.getElementById(direccion),
+    Numero: document.getElementById(numero),
+    Piso: document.getElementById(piso),
+    Escalera: document.getElementById(escalera),
+    FechaNacimiento: document.getElementById(fecha),
+    DniNie: document.getElementById(dni),
+    Prefijo: document.getElementById(prefijo),
+    TelefonoMovil: document.getElementById(tlf_movil),
+    TelefonoFijo: document.getElementById(tlf_fijo),
+    Iban: document.getElementById(cuenta_bancaria),
+    Swift: document.getElementById(swift)
+
+
+}
